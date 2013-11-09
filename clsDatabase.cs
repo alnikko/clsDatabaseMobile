066e099c50bc890c7c007d9f4a8bab513d4435d1
Alnikko Espina <nikkogeek@outlook.com>
2013-11-09 14:17:37 +0800

    Update

--- clsDatabase.cs
+++ clsDatabase.cs
@@ -31,6 +31,7 @@ namespace YourProjectName.Class
     }
     
     #region SQL Methods
+	
     public static bool OpenSqlConnection()
     {
         try
@@ -345,5 +346,259 @@ namespace YourProjectName.Class
             return result;
         }
     #endregion
+  
+	#region SQL CE Methods
+	public static bool CheckDataBaseExistance()
+        {
+            if (File.Exists("\\My Documents\\DatabaseName.sdf"))
+            {
+                return true;
+            }
+            else
+            {
+                try
+                {
+                    SqlCeEngine objSqlCeEngine = new SqlCeEngine(conStr_Local);
+                    objSqlCeEngine.CreateDatabase();
+                    CreateConfigTable();
+                    CreateDataHasChangedTable();
+                    CreateGroupsTable();
+                    CreateProductsTable();
+                    return true;
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+        }
+		
+		/* This will create the table for connecting to the MS SQL Database
+		 * This will function will execute when the app will be run on the first time.
+		*/
+        private static void CreateConfigTable()
+        {
+            SqlCeCommand cmd = new SqlCeCommand();
+            cmd.CommandText = "CREATE TABLE Config(IDN INT Primary Key,ServerName NVARCHAR(50),DatabaseName NVARCHAR(30),UserID NVARCHAR(30),Password NVARCHAR(30), ConnectionTimeout INT)";
+            cmd.Connection = objSqlCeConnection;
+            try
+            {
+                objSqlCeConnection.Open();
+                cmd.ExecuteNonQuery();
+				
+				/* Here Configure Default Insert Values
+				 * IDN = 1
+				 * ServerName = <ServerName or IP>, <PortID> ex. <192.168.1.1, 8080>
+				 * DatabaseName = <Name of The Database in the main SQL DATABASE>
+				 * UserID = <Your UserID in the main SQL Database>
+				 * Password = <Password to SQL Database>
+				 * ConnectionTimeout = <Default is 10>
+				*/
+                cmd.CommandText = "Insert Config(IDN,ServerName,DatabaseName,UserID,Password,ConnectionTimeout) Values(1,'ServerName/IP,8011','DatabaseName','sa','P@ssw0rd',10)";
+                cmd.Connection = objSqlCeConnection;
+                cmd.ExecuteNonQuery();
+            }
+            catch (SqlCeException ex)
+            {
+            }
+            finally
+            {
+                objSqlCeConnection.Close();
+            }
+        }
+
+        private static string GetSQLRemoteConnectionString()
+        {
+            string strRemoteConStr = "";
+            string serverName = "";
+            string database = "";
+            string userId = "";
+            string password = "";
+            string connectionTimeout = "";
+            try
+            {
+                SqlCeCommand cmd = new SqlCeCommand();
+                cmd.CommandText = "SELECT ServerName,DatabaseName,UserID,Password,ConnectionTimeout FROM Config";
+                cmd.Connection = objSqlCeConnection;
+                if (OpenSqlCeConnection())
+                {
+                    SqlCeDataReader objReader = cmd.ExecuteReader();
+                    if (objReader.Read())
+                    {
+                        serverName = objReader["ServerName"].ToString();
+                        database = objReader["DatabaseName"].ToString();
+                        userId = objReader["UserID"].ToString();
+                        password = objReader["Password"].ToString();
+                        connectionTimeout = objReader["ConnectionTimeout"].ToString();
+                    }
+                    objReader.Close();
+                    strRemoteConStr = "Data Source=" + serverName + "; Initial Catalog=" + database + "; User ID=" + userId + "; Password=" + password + "; Connection Timeout=" + connectionTimeout + "";
+                }
+            }
+            catch (SqlCeException ex)
+            { }
+            finally
+            {
+                CloseSqlCeConnection();
+            }
+            return strRemoteConStr;
+        }
+
+        public static DataSet ExecuteCeDataSet(string commandText)
+        {
+            DataSet ds = new DataSet();
+            try
+            {
+                SqlCeDataAdapter da = new SqlCeDataAdapter();
+                SqlCeCommand cmd = new SqlCeCommand();
+                cmd.CommandText = commandText;
+                cmd.Connection = objSqlCeConnection;
+                da.SelectCommand = cmd;
+                da.Fill(ds);
+            }
+            catch (SqlCeException ex)
+            {
+                //
+            }
+            return ds;
+        }
+
+        public static int ExecuteCeNonQuary(string _strCommandText)
+        {
+            int result = 0;
+            SqlCeCommand cmd = new SqlCeCommand();
+            cmd.CommandText = _strCommandText;
+            cmd.Connection = objSqlCeConnection;
+            try
+            {
+                OpenSqlCeConnection();
+                result = cmd.ExecuteNonQuery();
+                CloseSqlCeConnection();
+            }
+            catch (SqlCeException ex)
+            {
+                CloseSqlCeConnection();
+            }
+            return result;
+        }
+
+        public static object ExecuteCeScaler(string _strCommandText)
+        {
+            SqlCeCommand cmd = new SqlCeCommand();
+            cmd.CommandText = _strCommandText;
+            cmd.Connection = objSqlCeConnection;
+            try
+            {
+                OpenSqlCeConnection();
+                object result = Convert.ToInt32(cmd.ExecuteScalar());
+                return result;
+            }
+            catch (SqlCeException ex)
+            {
+                CloseSqlCeConnection();
+                return null;
+            }
+            finally
+            {
+                CloseSqlCeConnection();
+            }
+        }
+
+        public static bool OpenSqlCeConnection()
+        {
+            try
+            {
+                if (objSqlCeConnection.State == System.Data.ConnectionState.Closed)
+                {
+                    objSqlCeConnection.Open();
+                    return true;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            catch (SqlCeException)
+            {
+                return false;
+            }
+        }
+
+        public static bool CloseSqlCeConnection()
+        {
+            try
+            {
+                if (objSqlCeConnection.State == System.Data.ConnectionState.Open)
+                {
+                    objSqlCeConnection.Close();
+                    return true;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            catch (SqlCeException)
+            {
+                return false;
+            }
+        }
+
+        public static bool DeleteTableRows(string tableName)
+        {
+            SqlCeCommand cmd = new SqlCeCommand();
+            cmd.CommandText = "DELETE FROM "+tableName+"";
+            cmd.Connection = objSqlCeConnection;
+            try
+            {
+                objSqlCeConnection.Open();
+                cmd.ExecuteNonQuery();
+                return true;
+            }
+            catch (SqlCeException ex)
+            {
+                return false;
+            }
+            finally
+            {
+                objSqlCeConnection.Close();
+            }
+        }
+
+        public static SqlCeDataReader ExecuteCeReader(string _strCommand)
+        {
+            SqlCeDataReader objReader=null ;
+            SqlCeCommand cmd = new SqlCeCommand();
+            cmd.CommandText = _strCommand;
+            cmd.Connection = objSqlCeConnection;
+            try
+            {
+                objReader = cmd.ExecuteReader(System.Data.CommandBehavior.CloseConnection);
+            }
+            catch (SqlCeException)
+            { }
+            return (objReader);
+        }
+
+        public static bool CheckCeTableExistance(string tableName)
+        {
+            string commandText = "Select * from " + tableName;
+            SqlCeCommand objSqlCeCommand = new SqlCeCommand(commandText, objSqlCeConnection);
+            try
+            {
+                OpenSqlCeConnection();
+                objSqlCeCommand.ExecuteScalar();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+            finally
+            {
+                CloseSqlCeConnection();
+            }
+        }
+	#endregion
   }
 }
